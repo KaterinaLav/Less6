6bf64b89f725e9dcd51732ee6029aa0532452b03
LaSta <LavruhinaEA@yandex.ru>
2022-12-20 22:55:09 +0300

    выполнео

---
+++ src/Lesson6/ExampleMain.java
@@ -0,0 +1,37 @@
+package Lesson6;
+
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+import java.io.*;
+
+public class ExampleMain {
+    public static void main(String[] args) throws IOException {
+
+        OkHttpClient client = new OkHttpClient();
+
+        // Сегментированное построение URL
+        HttpUrl url = new HttpUrl.Builder()
+                .scheme("http")
+                .host("accuweather.com")
+                .addPathSegment("forecasts")
+                .addPathSegment("v1")
+                .addPathSegment("daily")
+                .addPathSegment("5day")
+                .addPathSegment("474212_PC")
+                .addQueryParameter("apikey", "w8B1ieDggRj3l5sDp9DCnF3G2trgPkv6")
+                .build();
+
+        System.out.println(url.toString());
+
+        // При необходимости указать заголовки
+        Request requesthttp = new Request.Builder()
+                .addHeader("accept", "application/json")
+                .url(url)
+                .build();
+
+        String jsonResponse = client.newCall(requesthttp).execute().body().string();
+        System.out.println(jsonResponse);
+    }
+}
